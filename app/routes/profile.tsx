661e26e7c255f4c90b3db892eb1870f8ae4d22d5
thawatchai-k <thawatchai.kru@rmutto.ac.th>
2024-11-30 12:45:32 +0700

    bouy

---
+++ app/routes/profile.tsx
@@ -0,0 +1,27 @@
+function Myprofil() {
+    return(
+        <div>
+            <h1>
+                Thawatchai Krutnaum
+            </h1>
+            <strong>
+                026740491802-6
+            </strong>
+        </div>
+        
+    );
+}
+function MyEducation() {
+    return(
+        <div>
+            <ul>
+                <li>Information Technology, RMUTTO, 2567-Present</li>
+                <li>9999</li>
+                <li>9999</li>
+
+                
+            </ul>
+        </div>
+    );
+}
+export default Myprofil;
